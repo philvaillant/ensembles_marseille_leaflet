88a8b3b1b7d8644212805ea1a8a6fadf8907a453
Philippe Vaillant <philippe.vaillant90@gmail.com>
2023-09-01 08:42:23 +0200

    quelques ajustements de style et texte d'accueil

--- public/javascripts/sidebar-v2.js
+++ public/javascripts/sidebar-v2.js
@@ -14,7 +14,7 @@ sidebar
         id: 'home',
         tab: '<i class="fa fa-scroll"></i>',
         title: 'Accueil',
-        pane: '<p>Description du projet<p>',
+        pane: '<div class="texte-accueil"><p>Cette carte interactive donne à voir 80 grands ensembles de plus de 100 logements construits à Marseille entre 1955 et 1975. </p><p><strong>Les données utilisées (dont les photographies) sont issues d’une étude commanditée par la direction régionale des affaires culturelles Provence-Alpes-Côte d’Azur (DRAC PACA), et réalisée en 2005 par Thierry Durousseau, architecte et urbaniste. </strong>Il ne s’agit que de valoriser sous une forme cartographiques les résultats très riches de cette étude qui ont par ailleurs été mis en ligne sur le <a href="https://www.culture.gouv.fr/Regions/Drac-Provence-Alpes-Cote-d-Azur/Politique-et-actions-culturelles/Architecture-contemporaine-remarquable-en-Paca/Les-etudes/Marseille-ensembles-et-residences-de-la-periode-1955-1975" target="_blank">site de la DRAC PACA</a> . </p><p>Les 80 résidences représentées sur la carte constituent un échantillon des 480 ensembles construits entre 1955 et 1975 répertoriés par l’étude. Il s’agit d’ensembles de logements sociaux, privés ou mixtes.</p> <p><strong>Notre volonté, en créant ce site, était de pouvoir faciliter l’identification de ces grands ensembles pour mieux apprivoiser le paysage urbain atypique et unique qu’ils forment.</strong> Un peu à la manière d’un randonneur qui chercherait à nommer les sommets d’une chaîne de montagnes pour mieux s’y aventurer.</p> <p>Les noms des cités qui s’affichent dans la liste déroulante, à gauche, sont ceux qui sont affichés à l’écran. La liste change donc à mesure que vous vous déplacez sur la carte.</p></div>',
     })
     .addPanel({
         id: 'fiches',
@@ -26,7 +26,7 @@ sidebar
         id: 'about',
         tab: '<i class="fa fa-question"></i>',
         title: 'Contact',
-        pane: '<p>Contact : XXX<p>',
+        pane: '<div><p>Nous sommes Mégane Aussedat et Philippe Vaillant, deux passionnés des sciences urbaines travaillant dans le domaine du logement. Vous pouvez nous contacter via X ou Linkedin.</p></div>',
     })
 sidebar.open('home');
 
@@ -34,14 +34,12 @@ function initializeContentSidebar(dataArray) {
     // var htmlList = '';
     listContent = '';
     dataArray.forEach((building) => {
-        // Remove img for now because of problems with the website
         listContent += '<div class="button-6 fiche-title" id="' + building.Titre + '"><h1>' + building.Titre + '</h1><div class="fiche-adresse">' + building.Adresse + ' 130' + building.arrondissement + ' Marseille' + '</div><img src="' + building.Image + '"><div class="fiche-info"><h3>Programme</h3><p>' + building.Programme +'</p></div><div class="fiche-info"><h3>Dates de construction</h3><p>' + building['Dates de construction'] +'</p></div><div class="fiche-info"><h3>Architecte</h3><p>' + building.Architecte +'</p></div><div class="fiche-info"><h3>Propriétaire</h3><p>' + building.Propriétaire +'</p></div><div class="fiche-info"><h3>Bâti</h3><p>' + building.Bâti +'</p></div><div class="link-details"><a href="' + building['Lien détail'] + '" target="_blank">Lien vers la page de détail sur le site du Ministère de la Culture</a></div><button class="bouton-page-details">Fermer la fiche de détail</button></div>'
     });
     // Il faut seulement afficher le contenu si on est à la page de liste
     if (!selectedFiche) {
         document.getElementById("ensembleinfo").innerHTML = listContent;
     };
-    // listContent = htmlList;
 };
 
 document.getElementById("fiches").addEventListener('mouseover', function (e) {
@@ -56,7 +54,6 @@ document.getElementById("fiches").addEventListener('mouseout', function (e) {
     }
 });
 
-// Il faut séparer le click sur un titre et le click sur un bouton de fermeture en fait / En fait non il faut juste remettre bien le display à none quand ça n'apparaît pas
 document.getElementById("fiches").addEventListener('click', function (e) {
     const element = e.target;
     if (element.classList.contains('fiche-title')) {
@@ -65,16 +62,10 @@ document.getElementById("fiches").addEventListener('click', function (e) {
     else if (element.classList.contains('bouton-page-details')) {
         goToList();
     }
-    // console.log(e.target.id);
-    // console.log(e.target);
-    // e.target.classList.add('visible');
-    // console.log(e.target.getAttribute("value"));
-    // selectedFiche = e.target.id;
-    // e.id contains the id of the opened panel
 });
 
 function showDetails(ficheId) {
-    document.getElementById(ficheId).classList.remove('button-6');
+    // document.getElementById(ficheId).classList.remove('button-6');
     document.getElementById('ensembleinfo').classList.add('see-details');
     if (selectedFiche) {
         document.getElementById(selectedFiche).classList.remove('visible');
@@ -92,7 +83,6 @@ function goToList() {
     markersObject[selectedFiche].setStyle(unclickStyle);
     selectedFiche = false;
     document.getElementById("ensembleinfo").innerHTML = listContent;
-    // document.getElementById(selectedFiche).classList.remove('visible');
 }
 
 function showSidebarInfo(markerId) {
